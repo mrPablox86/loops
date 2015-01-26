691df6540d96a77a0b9c79d8abf365876642748a
x86 <ya.freelan@yandex.ru>
2015-01-26 16:39:12 +0300

    create method MoveOutFromEmptyMaze() started create method MoveOutFromSnakeMaze()

--- Mazes/MazeTasks.cs
+++ Mazes/MazeTasks.cs
@@ -12,14 +12,56 @@ namespace Mazes
 		*/
 		public static void MoveOutFromEmptyMaze(Robot robot, int width, int height)
 		{
+            while(true)
+            {
+                for (var i = 0; i < 6; i++)
+                {
+                    robot.MoveTo(Direction.Down);
+                }
+
+                for(var j = 0; j < 5; j++)
+                {
+                    robot.MoveTo(Direction.Right);
+                }
+
+                return;
+            }
 		}
 
 		public static void MoveOutFromSnakeMaze(Robot robot, int width, int height)
 		{
+            var k = 0;
+            while(k != 6)
+            {
+                MoveSnake(robot, Direction.Right);
+
+                MoveSnakeDown(robot, Direction.Down);
+
+                MoveSnake(robot, Direction.Left);
+
+                k++;
+            }
+            MoveSnakeDown(robot, Direction.Down);
 		}
 
+        public static void MoveSnakeDown(Robot robot, Direction dir)
+        {
+            for (var j = 0; j < 2; j++)
+            {
+                robot.MoveTo(dir);
+            }
+        }
+
+        public static void MoveSnake(Robot robot, Direction dir)
+        {
+             for ( var i = 0; i < 25; i++)
+             {
+                 robot.MoveTo(dir);
+             }
+        }
+
 		public static void MoveOutFromPyramidMaze(Robot robot, int width, int height)
-		{
+		{ 
 		}
 	}
 }
