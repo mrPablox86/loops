cacf221a972b5b59b880176081d624db64312810
x86 <ya.freelan@yandex.ru>
2015-01-26 14:41:38 +0300

    Create new project Loops.sln

---
+++ Mazes/MazeTasks.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Mazes
+{
+	public static class MazeTasks
+	{
+		/*
+		* Не нужно писать код выхода из произвольного лабиринта. Напишите, решение для конкретных лабиринтов.
+		* Используйте циклы. Ваш К.О.
+		* Постарайтесь решить задачу с ограничение "не более одного цикла на метод".
+		* Подумайте, какие вспомогательные методы помогут сделать ваше решение более лаконичным, понятным и красивым.
+		*/
+		public static void MoveOutFromEmptyMaze(Robot robot, int width, int height)
+		{
+		}
+
+		public static void MoveOutFromSnakeMaze(Robot robot, int width, int height)
+		{
+		}
+
+		public static void MoveOutFromPyramidMaze(Robot robot, int width, int height)
+		{
+		}
+	}
+}
---
+++ Mazes/Robot.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+
+namespace Mazes
+{
+	public enum Direction
+	{
+		Up,
+		Down,
+		Left,
+		Right
+	}
+
+	public class Robot
+	{
+		private readonly Maze maze;
+		private readonly List<Point> path = new List<Point>();
+
+		public Robot(Maze maze)
+		{
+			this.maze = maze;
+			path.Add(maze.Robot);
+		}
+
+		public IEnumerable<Point> Path { get { return path; } }
+
+		public Point Pos
+		{
+			get { return path.Last(); }
+		}
+
+		public void MoveTo(Direction dir)
+		{
+			int[] dxs = { 0, 0, -1, 1 };
+			int[] dys = { -1, 1, 0, 0 };
+			TryMoveTo(new Point(
+				Pos.X + dxs[(int)dir], 
+				Pos.Y + dys[(int)dir]));
+		}
+
+		private void TryMoveTo(Point destination)
+		{
+			if (path.Count > 1000 || maze.IsWall(destination))
+				throw new Exception("Robot is broken!");
+			else path.Add(destination);
+		}
+	}
+}
